c9c991f2550da70ddbafa8bb63409b822bcd5843
jst1994 <jst_1994@qq.com>
2015-06-21 09:03:23 +0800

    first commit

---
+++ main.cpp
@@ -0,0 +1,115 @@
+#include <iostream>
+#include <string.h>
+
+using namespace std;
+
+struct Computer
+{
+    char model[20];
+    int total;
+}computers[100];
+
+int modelCount=0;
+
+void list();
+void enterwarehouse();
+void outwarehouse();
+void input(struct Computer &computer);
+int find(char* model);
+
+int main()
+{
+    int action;
+    do
+    {
+        cout<<"---------菜单----------"<<endl;
+        cout<<"1）查看库存"<<endl;
+        cout<<"2)电脑入库"<<endl;
+        cout<<"3)售出"<<endl;
+        cout<<"4)退出程序"<<endl;
+
+        cin>>action;
+        switch(action)
+        {
+            case 1:
+                list();
+                break;
+            case 2:
+                enterwarehouse();
+                break;
+            case 3:
+                outwarehouse();
+                break;
+            case 4:
+                return 0;
+            default:
+                cout<<"您输入了错误的菜单项，请重新选择！";
+        }
+
+
+
+    }while(1);
+}
+
+
+void list()
+{
+    cout<<"-------库存-------"<<endl;
+    cout<<"型号\t"<<"数量"<<endl;
+    for(int i=0;i<modelCount;i++)
+    {
+        cout<<computers[i].model<<"\t"<<computers[i].total<<endl;
+    }
+    cout<<"------库存------"<<endl;
+}
+void enterwarehouse()
+{
+    struct Computer computer;
+    input(computer);
+
+    int i=find(computer.model);
+    if(i==modelCount)
+    {
+        (computers[i].model,computer.model);
+        computers[i].total=computer.total;
+        modelCount++;
+    }
+    else
+    {
+      computers[i].total+=computer.total;
+    }
+}
+
+void outwarehouse()
+{
+    struct Computer computer;
+    input(computer);
+
+    int i=find(computer.model);
+    if(i==modelCount)
+    {cout<<"型号错误!"<<endl;}
+    else
+    {
+      computers[i].total-=computer.total;
+    }
+}
+void input(struct Computer &computer)
+{
+	cout<<"型号：";
+	cin>>computer.model;
+
+	cout<<"数量：";
+	cin>>computer.total;
+}
+
+int find(char* model)
+{
+	int i;
+	for(i=0;i<modelCount;i++){
+		if(strcmp
+     (computers[i].model,model) == 0){
+			break;
+		}
+	}
+	return i;
+}
